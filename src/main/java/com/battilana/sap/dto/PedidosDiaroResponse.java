d2a2db75e2def6edaebcd6828f5b101732f4c1ec
Fredy <inztockz@gmail.com>
2026-07-16 16:02:28 -0500

    update

--- src/main/java/com/battilana/sap/dto/PedidosDiaroResponse.java
+++ src/main/java/com/battilana/sap/dto/PedidosDiaroResponse.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 
 public record PedidosDiaroResponse(
         Integer docEntry,
-        Short docTime,
+        Integer docTime,
         LocalDateTime fechaCreacionPedido,
         String cardCode,
         String cardName,
